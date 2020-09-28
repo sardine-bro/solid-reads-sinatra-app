2057cc50d9d31f2f31e2bff645e85c16e1844113
Teodoro Garcia <teodorogarcia@teodoros-air.lan>
2020-09-28 18:06:04 -0500

    implemented login functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
    
     enable :sessions 
-    # set :session_secret
+    set :session_secret, "super_duper_secret_guest_1234"
   end
 
   get "/" do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
-    get "/login" do 
-        
+    
+    # Read
+    get "/login" do  
         erb :login
+    end
+
+    post "/login" do 
+        # puts params 
+        @user = User.find_by(email: params[:email]) 
+
+        if user && user.authenticate(params[:password])
+            # users home page
+            # binding.pry
+            session[:user_id] = @user.id
+            redirect "/users/#{user.id}"
+        else
+            redirect '/login'
+        end
+    end
+
+    get "/users/:id" do 
+        "Users SHow page"
     end
 
 end
