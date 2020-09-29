d246f2c5fb413f5f4503f8356ba0c6a7da6dd75f
Teodoro Garcia <teodorogarcia@Teodoros-MacBook-Air.local>
2020-09-29 18:18:50 -0500

    added CREATE functionality to the posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,22 +2,37 @@ class PostsController < ApplicationController
 
 # initiate CRUD
 
-# R
+# R/Index
     get '/posts' do 
         @posts = Post.all
         
         erb :'/posts/index'
     end
 
+# C/New
+    get '/posts/new' do 
+        erb :'/posts/new'
+    end
+
+    post '/posts' do
+        post = Post.create(title: params[:title], review: params[:review], user_id: current_user.id)
+
+        redirect "/posts/#{post.id}"
+    end
+
+# R/Show
     get '/posts/:id' do 
-        # binding.pry
-       @post = Post.find(params[:id])
+        @post = Post.find(params[:id])
 
        erb :'/posts/show'
     end
-# C
 
+    
 # U
+    get '/articles/:id/edit' do  #load edit form
+        @article = Article.find_by_id(params[:id])
+        erb :edit
+    end
 
 # D
 end
