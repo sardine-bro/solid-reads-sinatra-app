c07352f96e551e29e22e85ba8a190a1719919d92
Teodoro Garcia <teodorogarcia@Teodoros-MacBook-Air.local>
2020-09-29 14:36:00 -0500

    added seed data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,9 @@ class UsersController < ApplicationController
     post "/login" do 
         @user = User.find_by(email: params[:email]) 
 
-        if user && user.authenticate(params[:password])
+        if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
-            redirect "/users/#{user.id}"
+            redirect "/users/#{@user.id}"
         else
             redirect '/login'
         end
---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+craig = User.create(name: "Craig", email: "craig@meow.com", profile_bio: "Just a cool cat who loves reading and writing", password: "guest")
+kona = User.create(name: "Kona", email: "kona@meow.com", profile_bio: "Just as cool cat who also loves reading and writing", password: "guest")
+
+Post.create(title: "A Day in the Life of Meow", review: "It was truly early in the morning when lorem ipsum the lorem ipsum swiggity swag whoot I am a cool cat meow mewo.", user_id: craig.id)
+Post.create(title: "A Real Day in the Life of Meow", review: "It was even earlier in the morning when lorem ipsum the lorem ipsum swiggity swag whoot I am a cool cat meow me0w.", user_id: kona.id)
